e8e297ad420868ae0af6b2b871cde39f190d3917
bongomin <candanobongomindaniel@gmail.com>
2020-06-10 14:44:03 +0300

    Initial Commit

---
+++ Chapter2/SandBox.js
@@ -0,0 +1,42 @@
+// let firstName = "Bongomin";
+// let seconName = "Daniel";
+// let email = "bongomindaniel@gmail.com"
+
+// let fullName;
+// fullName = `${firstName} ${seconName}`;
+// let result = fullName.toUpperCase()
+// // length of full name
+// let length = fullName.length;
+// console.log(result)
+// console.log(length);
+
+// console.log(email.indexOf('@'));
+
+// const splitName = fullName.split(" ");
+// console.log(splitName)
+// console.log(splitName[0]);
+// console.log(splitName[1]);
+
+// const intercahnge = `${splitName[1]} ${splitName[0]}`
+// console.log(intercahnge)
+
+
+// // playing with methods
+// let resultOne = email.slice(1, 3);
+// console.log(resultOne);
+
+// const replace = email.replace("b", "B");
+
+// console.log(replace);
+
+let radius = 10;
+const pi = 3.14;
+
+console.log(radius, pi)
+
+const numbers = [1, 2, 3, 4, 45, 6]
+
+console.log(numbers)
+console.log(numbers[4])
+numbers[2] = "me";
+console.log(numbers)
---
+++ Chapter3/SandBox.js
@@ -0,0 +1,25 @@
+// conditional statements
+
+// if else statement
+// syntax
+// if(condition){
+//    statement ...1
+// }else{
+//    statement ...2
+// }
+
+// loops
+
+// for lo0p
+
+// syntax
+
+// for (initial , condition, action){
+//    statement..
+// }
+
+const names = ["Bongomin", "Daniel", "Akena", "Ouma", "Kilama"]
+for (let a = 0; a < names.length; a++) {
+   let html = `<div>${names[a]}</div>`
+   console.log(html)
+}
