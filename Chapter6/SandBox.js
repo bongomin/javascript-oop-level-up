2c69631756b11a78dac147cf36daa1517aa02711
bongomin <candanobongomindaniel@gmail.com>
2020-06-13 17:36:49 +0300

    feat(Dom Maniplation):
    - use of selectQuery
    
    - Use of getAtribute from the dom
    
    - use of addAtribute from the Dom
    
    - Use of the SelectAtribute propert

--- Chapter6/SandBox.js
+++ Chapter6/SandBox.js
@@ -4,3 +4,10 @@
 // Adding, Change & Delete Content
 // making a cool Pop up in Javascript
 
+
+const para = document.querySelector('p');
+console.log(para)
+const error = document.querySelector('.error');
+console.log(error)
+const errorOne = document.querySelector('div.error');
+console.log(errorOne)
